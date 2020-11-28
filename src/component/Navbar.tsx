9e29a238585592a7b1e7c6f973386d13a00fd25e
Yuki Matsuo <yukimaterrace@gmail.com>
2020-11-28 19:24:46 +0900

    update coding

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 
-function Navbar() {
-    return (
+const Navbar = () =>
+    (
         <nav className="navbar navbar-light bg-light">
             <span className="navbar-brand mb-0 h1">Andaman UI</span>
         </nav>
     )
-}
 
 export default Navbar
