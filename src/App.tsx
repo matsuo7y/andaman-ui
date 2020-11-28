859a923cd8b70a5b1b05997577d1c6978c7b25c0
Yuki Matsuo <yukimaterrace@gmail.com>
2020-11-28 19:18:23 +0900

    initial app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './component/Navbar'
+
+function App() {
+  return (
+    <div>
+      <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
